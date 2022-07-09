e80edb1632edebaee705847acbb5cf27d794601a
Dyana Varghese <dyna1.arackal@gmail.com>
2022-07-09 12:40:40 +0100

    changed base url to https

--- lib/ApiCalls/routes.dart
+++ lib/ApiCalls/routes.dart
@@ -1,6 +1,6 @@
 class Routes {
   String baseUrl =
-      "http://makeup-api-backend-flask.herokuapp.com"; //"http://makeup-api-backend-flask.herokuapp.com";
+      "https://makeup-api-backend-flask.herokuapp.com"; //"http://makeup-api-backend-flask.herokuapp.com";
 
   String login = "/login";
   String signup = "/signup";
